f60650a093d9959cb2a7bb92d78bd567d3bf33de
MYPC\kimseungha <bookcon23@gmail.com>
2026-05-04 20:35:35 +0900

    김승하 / 게임모드 블루프린트 클래스 생성 및 전투 컴포넌트 완성

---
+++ Source/TeamProject/BattleSystemComponent.cpp
@@ -0,0 +1,39 @@
+﻿// BattleSystemComponent.cpp
+
+
+#include "BattleSystemComponent.h"
+
+UBattleSystemComponent::UBattleSystemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UBattleSystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UBattleSystemComponent::DamageCalculation(float attackerATK)
+{
+	if (!isDead)
+	{
+		HP = HP - attackerATK;
+
+		if (HP <= 0)
+		{
+			HP = 0;
+			isDead = true; // TODO: 사망 델리게이트 Broadcast, GameMode에게 전달
+		}
+	}
+}
+
+void UBattleSystemComponent::Heal()
+{
+	HP = FMath::Min(HP + ATK * 0.45f, MaxHP);
+}
+
+
+
+
+
---
+++ Source/TeamProject/BattleSystemComponent.h
@@ -0,0 +1,36 @@
+﻿//BattleSystemComponent.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BattleSystemComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TEAMPROJECT_API UBattleSystemComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UBattleSystemComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float HP;
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHP;
+	UPROPERTY(EditDefaultsOnly)
+	float ATK;
+	UPROPERTY()
+	bool isDead;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void DamageCalculation(float attackerATK);
+	UFUNCTION(BlueprintCallable)
+	void Heal();
+};
