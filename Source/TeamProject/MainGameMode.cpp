90b8f9a2b20ead891d2734c6d24284dacfd67ccd
MYPC\kimseungha <bookcon23@gmail.com>
2026-05-04 12:45:46 +0900

    GameMode 완성 및 Build.cs 모듈 추가

--- Source/TeamProject/MainGameMode.cpp
+++ Source/TeamProject/MainGameMode.cpp
@@ -1,5 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MainGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "BluePrint/UserWidget.h"
 
+void AMainGameMode::SetGameOver()
+{
+	UGameplayStatics::SetGamePaused(GetWorld(), true); 
+	
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+
+	if (PC != nullptr)
+	{
+		UUserWidget* GameOverWidget = CreateWidget<UUserWidget>(PC, GameOverWidgetClass);
+
+		if (GameOverWidget != nullptr)
+		{
+			GameOverWidget->AddToViewport();
+		}
+	}
+}
